68558223afc11591f42c18dd7dce32aacbafc580
R Aditaya <govthealth720@gmail.com>
2026-03-07 04:57:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, send_from_directory
 import requests
 from PIL import Image
 from io import BytesIO
+import urllib.parse
 
 app = Flask(__name__, static_folder=".")
 
@@ -12,15 +13,26 @@ def home():
 @app.route("/generate", methods=["POST"])
 def generate():
 
-    prompt = request.json["prompt"]
+    data = request.get_json()
+    prompt = data["prompt"]
 
-    url = f"https://image.pollinations.ai/prompt/{prompt}"
+    # prompt encode
+    prompt_encoded = urllib.parse.quote(prompt)
+
+    url = f"https://image.pollinations.ai/prompt/{prompt_encoded}"
+
     response = requests.get(url)
 
     img = Image.open(BytesIO(response.content))
     img.save("output.png")
 
-    return jsonify({"image": "output.png"})
+    return jsonify({"image": "/output.png"})
+
+
+@app.route("/output.png")
+def get_image():
+    return send_from_directory(".", "output.png")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
